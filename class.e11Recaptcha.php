4f56027f2e71520404da698eeeb20e00645b7d89
Eric <eric@epsilon11.com>
2017-01-27 15:08:03 -0600

    Loaded plugin options to control comment reCAPTCHA

--- class.e11Recaptcha.php
+++ class.e11Recaptcha.php
@@ -20,6 +20,13 @@
 class e11Recaptcha {
   private static $initialized = false;
 
+  // Plugin options
+  
+  private static $siteKey;
+  private static $secretKey;
+  private static $behaviorComments;
+  private static $behaviorNewUsers;
+  
   public static function plugin_activation() {
 
   }
@@ -32,15 +39,25 @@ class e11Recaptcha {
    * Attach a reCAPTCHA field to the area after the "comment" field on comment
    * forms.
    *
-   * [TODO] Add admin interface to control whether field is used for all
-   * comments, for comments made without being logged in, or not at all.
-   *
    * @param array $comment_fields Associative array of comment field HTML
    *                              keyed by field name
    * @return array Filtered $comment_fields with "comment" field altered to
    *               include reCAPTCHA HTML
    */
   public static function comment_form_captcha($comment_fields) {
+
+    // Don't display reCAPTCHA if disabled on comments.
+
+    if (self::$behaviorComments == 'disabled') {
+      return $comment_fields;
+    }
+
+    // Don't display reCAPTCHA if only enabled when users are not logged
+    // in and user is logged in.
+
+    if (self::$behaviorComments == 'not_logged_in' && is_user_logged_in()) {
+      return $comment_fields;
+    }
 
     // Push reCAPTCHA field into the comment field code, to ensure it can
     // appear whether a user is logged in or not.
@@ -49,24 +66,77 @@ class e11Recaptcha {
       <p class="comment-form-e11-recaptcha">
         <label for="comment-form-e11-recaptcha">Recaptcha</label>
         <div id="comment-form-e11-recaptcha"></div>
-      </p>';
+      </p>
+      ';
 
     return $comment_fields;
   }
 
-  /**
-   * [TODO] Add admin interface to set Google ReCAPTCHA API key, and read it
-   *        in from database here.
-   */
   public static function init() {
+
+    // Ensure function is called only once.
+
     if (self::$initialized) {
       return;
     }
 
     self::$initialized = true;
 
-    wp_register_script('recaptcha-api-js', 'https://www.google.com/recaptcha/api.js' , '', '', true);
+    // Read plugin options, and set class variables from them.
+
+    $options = get_option('e11_recaptcha_options', array());
+
+    if (!isset($options['e11_recaptcha_field_site_key'])) {
+      self::$siteKey = '';
+    } else {
+      self::$siteKey = $options['e11_recaptcha_field_site_key'];
+    }
+
+    if (!isset($options['e11_recaptcha_field_secret_key'])) {
+      self::$secretKey = '';
+    } else {
+      self::$secretKey = $options['e11_recaptcha_field_secret_key'];
+    }
+
+    if (!isset($options['e11_recaptcha_field_behavior_comments'])) {
+      self::$behaviorComments = 'not_logged_in';
+    } else {
+      switch($options['e11_recaptcha_field_behavior_comments']) {
+        case 'all_comments':
+        case 'not_logged_in':
+        case 'disabled':
+          self::$behaviorComments =
+            $options['e11_recaptcha_field_behavior_comments'];
+          break;
+
+        default:
+          self::$behaviorComments = 'not_logged_in';
+          break;
+      }
+    }
+
+    if (!isset($options['e11_recaptcha_field_behavior_new_users'])) {
+      self::$behaviorNewUsers = 'enabled';
+    } else {
+      switch($options['e11_recaptcha_field_behavior_new_users']) {
+        case 'enabled':
+        case 'disabled':
+          self::$behaviorNewUsers =
+            $options['e11_recaptcha_field_behavior_new_users'];
+          break;
+
+        default:
+          self::$behaviorNewUsers = 'enabled';
+          break;
+      }
+    }
+
+    // Tell WordPress to load reCAPTCHA API script.
+
+    wp_register_script('recaptcha-api-js', 'https://www.google.com/recaptcha/api.js', '', '', true);
     wp_enqueue_script('recaptcha-api-js');
+
+    // Add plugin to WordPress hooks.
 
     add_filter('comment_form_fields', array('e11Recaptcha', 'comment_form_captcha'));
   }
